a22d03419cd82086c5b0632debcbd660ed7c1c05
Isaac.Lera <isaac.lera@gmail.com>
2023-04-03 20:25:08 +0200

    update

--- run_eval_nsgaII_exps.sh
+++ run_eval_nsgaII_exps.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-CASE="E500"
-DEV=499
-JOBS=6
+CASE="E1000v2"
+DEV=999
+JOBS=9
 
+rm *.out
+rm *.err
 python -u "generate_dataset.py" --name $CASE --typeDS VALIDATION --n_devices $DEV --n_jobs $JOBS --len_dataset 50 --np_seed_dataset 2023 &&
 python -u "generate_dataset.py" --name $CASE --typeDS TEST --n_devices $DEV --n_jobs $JOBS --len_dataset 30 --np_seed_dataset 1290 &&
 nohup python -u "eval_nsgaII_mono.py" --name $CASE --n_devices $DEV --n_jobs $JOBS --n_gen 100 --rewardWeightCost 0.5 --rewardWeightTime 0.5 > fM.out 2> feM.err < /dev/null &&
 nohup python -u "eval_nsgaII_mono.py" --name $CASE --n_devices $DEV --n_jobs $JOBS --n_gen 100 --rewardWeightCost 1. --rewardWeightTime 0. > fM2.out 2> feM2.err < /dev/null &&
 nohup python -u "eval_nsgaII_mono.py" --name $CASE --n_devices $DEV --n_jobs $JOBS --n_gen 100 --rewardWeightCost 0. --rewardWeightTime 1. > fL2.out 2> feL2.err < /dev/null &&
 nohup python -u "eval_nsgaII_mono.py" --name $CASE --n_devices $DEV --n_jobs $JOBS --n_gen 100 --rewardWeightCost 0.25 --rewardWeightTime 0.75 > fN2.out 2> feN2.err < /dev/null &&
-nohup python -u "eval_nsgaII_mono.py" --name $CASE --n_devices $DEV --n_jobs $JOBS --n_gen 100 --rewardWeightCost 0.75 --rewardWeightTime 0.25 > fK2.out 2> feK2.err < /dev/null &
+nohup python -u "eval_nsgaII_mono.py" --name $CASE --n_devices $DEV --n_jobs $JOBS --n_gen 100 --rewardWeightCost 0.75 --rewardWeightTime 0.25 > fK2.out 2> feK2.err < /dev/null &&
+touch DONE_PF.out &&
+nohup python -u "eval_nsgaII.py" --name $CASE --n_devices $DEV --n_jobs $JOBS --n_gen 100 > fPF.out 2> fPF.err < /dev/null &&
+touch DONE.out &
--- run_train_ppo_PF.sh
+++ run_train_ppo_PF.sh
@@ -1,13 +1,19 @@
 #!/bin/bash  
 
-CASE="E500"
-DEV=499
-JOBS=6
+CASE="E1000v2"
+DEV=999
+JOBS=9
 
+
+rm *.out &&
+rm *.err &&
 python -u "generate_dataset.py" --name $CASE --typeDS VALIDATION --n_devices $DEV --n_jobs $JOBS --len_dataset 50 --np_seed_dataset 2023 &&
 python -u "generate_dataset.py" --name $CASE --typeDS TEST --n_devices $DEV --n_jobs $JOBS --len_dataset 30 --np_seed_dataset 1290 &&
 nohup python -u "train_ppo.py" --name $CASE --n_devices $DEV --n_jobs $JOBS --max_updates 150 --device cuda > fp55.out 2> fp55.err < /dev/null &&
 nohup python -u "train_ppo.py" --name $CASE --n_devices $DEV --n_jobs $JOBS --max_updates 150 --device cpu --rewardWeightCost 1.0 --rewardWeightTime 0.0 --device cuda --record_alloc_episodes 0 10 20 30  > fp01.out 2> fp01.err < /dev/null &&
 nohup python -u "train_ppo.py" --name $CASE --n_devices $DEV --n_jobs $JOBS --max_updates 100 --device cpu --rewardWeightCost 0.0 --rewardWeightTime 1.0 --device cuda --record_alloc_episodes 0 10 20 30 40 > fp10.out 2> fp10.err < /dev/null &&
 nohup python -u "train_ppo.py" --name $CASE --n_devices $DEV --n_jobs $JOBS --max_updates 150 --device cpu --rewardWeightCost 0.75 --rewardWeightTime 0.25 --device cuda > fp27.out 2> fp27.err < /dev/null &&
-nohup python -u "train_ppo.py" --name $CASE --n_devices $DEV --n_jobs $JOBS --max_updates 150 --device cpu --rewardWeightCost 0.25 --rewardWeightTime 0.75 --device cuda > fp72.out 2> fp72.err < /dev/null &
+nohup python -u "train_ppo.py" --name $CASE --n_devices $DEV --n_jobs $JOBS --max_updates 150 --device cpu --rewardWeightCost 0.25 --rewardWeightTime 0.75 --device cuda > fp72.out 2> fp72.err < /dev/null &&
+touch DONE_1.out &&
+nohup python -u "eval_trained_PF_ppo.py" --name $CASE  --n_devices $DEV --n_jobs $JOBS > fpEND.out 2> fpEND.err < /dev/null &&
+touch DONE_2.out &
