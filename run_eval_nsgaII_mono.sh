989483a0cf71c860c9c718ad79031f50245ab5ea
Isaac.Lera <isaac.lera@gmail.com>
2023-04-03 13:25:13 +0200

    update

--- run_eval_nsgaII_mono.sh
+++ run_eval_nsgaII_mono.sh
@@ -4,6 +4,8 @@ CASE="E500"
 DEV=499
 JOBS=6
 
+python -u "generate_dataset.py" --name $CASE --typeDS VALIDATION --n_devices $DEV --n_jobs $JOBS --len_dataset 50 --np_seed_dataset 2023 &&
+python -u "generate_dataset.py" --name $CASE --typeDS TEST --n_devices $DEV --n_jobs $JOBS --len_dataset 30 --np_seed_dataset 1290 &&
 nohup python -u "eval_nsgaII_mono.py" --name $CASE --n_devices $DEV --n_jobs $JOBS --n_gen 100 --rewardWeightCost 0.5 --rewardWeightTime 0.5 > fM.out 2> feM.err < /dev/null &&
 nohup python -u "eval_nsgaII_mono.py" --name $CASE --n_devices $DEV --n_jobs $JOBS --n_gen 100 --rewardWeightCost 1. --rewardWeightTime 0. > fM2.out 2> feM2.err < /dev/null &&
 nohup python -u "eval_nsgaII_mono.py" --name $CASE --n_devices $DEV --n_jobs $JOBS --n_gen 100 --rewardWeightCost 0. --rewardWeightTime 1. > fL2.out 2> feL2.err < /dev/null &&
